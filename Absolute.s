c8bcb7afce9ebbef7aeec481fa7bf9dbe12109c7
pam024 <pam0424@gmail.com>
2022-03-11 00:42:32 -0500

    Class Exercise

---
+++ Absolute.s
@@ -0,0 +1,127 @@
+	.file	"Absolute.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"Absolute value: %d \n"
+	.text
+	.globl	abs_diff
+	.type	abs_diff, @function
+abs_diff:
+.LFB6:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$32, %rsp
+	movl	%edi, -20(%rbp)
+	movl	%esi, -24(%rbp)
+	movl	-20(%rbp), %eax
+	cmpl	-24(%rbp), %eax
+	jle	.L2
+	movl	-20(%rbp), %eax
+	subl	-24(%rbp), %eax
+	movl	%eax, %edx
+	movl	%edx, %eax
+	sarl	$31, %eax
+	xorl	%eax, %edx
+	movl	%edx, -4(%rbp)
+	subl	%eax, -4(%rbp)
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	jmp	.L3
+.L2:
+	movl	-24(%rbp), %eax
+	subl	-20(%rbp), %eax
+	movl	%eax, %edx
+	movl	%edx, %eax
+	sarl	$31, %eax
+	xorl	%eax, %edx
+	movl	%edx, -4(%rbp)
+	subl	%eax, -4(%rbp)
+	movl	-4(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+.L3:
+	movl	-4(%rbp), %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE6:
+	.size	abs_diff, .-abs_diff
+	.section	.rodata
+.LC1:
+	.string	"Enter two integers numbers: "
+.LC2:
+	.string	"%d"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB7:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	leaq	.LC1(%rip), %rdi
+	call	puts@PLT
+	leaq	-16(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC2(%rip), %rdi
+	movl	$0, %eax
+	call	__isoc99_scanf@PLT
+	leaq	-12(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC2(%rip), %rdi
+	movl	$0, %eax
+	call	__isoc99_scanf@PLT
+	movl	-12(%rbp), %edx
+	movl	-16(%rbp), %eax
+	movl	%edx, %esi
+	movl	%eax, %edi
+	call	abs_diff
+	movl	$0, %eax
+	movq	-8(%rbp), %rcx
+	xorq	%fs:40, %rcx
+	je	.L7
+	call	__stack_chk_fail@PLT
+.L7:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE7:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 9.3.0-17ubuntu1~20.04) 9.3.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
