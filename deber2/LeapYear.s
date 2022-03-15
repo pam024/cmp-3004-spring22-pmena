0c31518600c588a9417a746c83578fb901ffd74f
pam024 <90577085+pam024@users.noreply.github.com>
2022-03-15 18:31:08 -0500

    Final exercises hw2

---
+++ deber2/LeapYear.s
@@ -0,0 +1,126 @@
+	.file	"LeapYear.c"
+	.text
+	.section	.rodata
+.LC0:
+	.string	"Enter a year: "
+.LC1:
+	.string	"%d"
+.LC2:
+	.string	"%d is a leap year \n"
+.LC3:
+	.string	"%d is a not leap year \n"
+	.text
+	.globl	main
+	.type	main, @function
+main:
+.LFB0:
+	.cfi_startproc
+	endbr64
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movq	%fs:40, %rax
+	movq	%rax, -8(%rbp)
+	xorl	%eax, %eax
+	leaq	.LC0(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	leaq	-12(%rbp), %rax
+	movq	%rax, %rsi
+	leaq	.LC1(%rip), %rdi
+	movl	$0, %eax
+	call	__isoc99_scanf@PLT
+	movl	-12(%rbp), %edx
+	movslq	%edx, %rax
+	imulq	$1374389535, %rax, %rax
+	shrq	$32, %rax
+	movl	%eax, %ecx
+	sarl	$7, %ecx
+	movl	%edx, %eax
+	sarl	$31, %eax
+	subl	%eax, %ecx
+	movl	%ecx, %eax
+	imull	$400, %eax, %eax
+	subl	%eax, %edx
+	movl	%edx, %eax
+	testl	%eax, %eax
+	jne	.L2
+	movl	-12(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC2(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	jmp	.L3
+.L2:
+	movl	-12(%rbp), %edx
+	movslq	%edx, %rax
+	imulq	$1374389535, %rax, %rax
+	shrq	$32, %rax
+	movl	%eax, %ecx
+	sarl	$5, %ecx
+	movl	%edx, %eax
+	sarl	$31, %eax
+	subl	%eax, %ecx
+	movl	%ecx, %eax
+	imull	$100, %eax, %eax
+	subl	%eax, %edx
+	movl	%edx, %eax
+	testl	%eax, %eax
+	jne	.L4
+	movl	-12(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC2(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	jmp	.L3
+.L4:
+	movl	-12(%rbp), %eax
+	andl	$3, %eax
+	testl	%eax, %eax
+	jne	.L5
+	movl	-12(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC2(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+	jmp	.L3
+.L5:
+	movl	-12(%rbp), %eax
+	movl	%eax, %esi
+	leaq	.LC3(%rip), %rdi
+	movl	$0, %eax
+	call	printf@PLT
+.L3:
+	movl	$0, %eax
+	movq	-8(%rbp), %rsi
+	xorq	%fs:40, %rsi
+	je	.L7
+	call	__stack_chk_fail@PLT
+.L7:
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	main, .-main
+	.ident	"GCC: (Ubuntu 9.4.0-1ubuntu1~20.04) 9.4.0"
+	.section	.note.GNU-stack,"",@progbits
+	.section	.note.gnu.property,"a"
+	.align 8
+	.long	 1f - 0f
+	.long	 4f - 1f
+	.long	 5
+0:
+	.string	 "GNU"
+1:
+	.align 8
+	.long	 0xc0000002
+	.long	 3f - 2f
+2:
+	.long	 0x3
+3:
+	.align 8
+4:
